d41fb2a082098bd3e1e452632d2f7ed3737b376b
Tony Chao <tchao@mail.usf.edu>
2022-06-05 11:59:15 -0400

    Add routes for projectBoard and projectTaskForm component

--- project-management-tool-react-client/src/App.js
+++ project-management-tool-react-client/src/App.js
@@ -3,24 +3,32 @@ import { Route, Switch } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
 import ProjectForm from "./components/project/ProjectForm";
+import ProjectBoard from "./components/project-board/ProjectBoard";
+import ProjectTaskForm from "./components/project-board/project-task/ProjectTaskForm";
 import Layout from "./ui/Layout";
 
 const App = () => {
   return (
     <Layout>
       <Switch>
-      <Route path="/dashboard" exact>
-        <Dashboard />
-      </Route>
-      <Route path="/project-form" exact>
-        <ProjectForm />
-      </Route>
-      <Route path="/project-form/:projectId" exact>
-        <ProjectForm />
-      </Route>
+        <Route path="/dashboard" exact>
+          <Dashboard />
+        </Route>
+        <Route path="/project-form" exact>
+          <ProjectForm />
+        </Route>
+        <Route path="/project-form/:projectId" exact>
+          <ProjectForm />
+        </Route>
+        <Route path="/project-board/:projectId" exact>
+          <ProjectBoard />
+        </Route>
+        <Route path="/project-task-form/:projectId" exact>
+          <ProjectTaskForm />
+        </Route>
       </Switch>
     </Layout>
   );
-}
+};
 
 export default App;
