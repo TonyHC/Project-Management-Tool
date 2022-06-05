da76af6ebf77465350fc8c7b3bfff04692dab3ce
Tony Chao <tchao@mail.usf.edu>
2022-06-05 19:36:38 -0400

    Update project list component to display correct tasks for each project status

--- project-management-tool-react-client/src/components/project-board/project-task/ProjectTaskItem.js
+++ project-management-tool-react-client/src/components/project-board/project-task/ProjectTaskItem.js
@@ -2,15 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const ProjectTaskItem = (props) => {
+  let priority;
+  let cardHeaderStyles;
+
+  if (props.priority === 1) {
+    priority = "HIGH";
+    cardHeaderStyles = "card-header bg-danger";
+  } else if (props.priority === 2) {
+    priority = "MEDIUM";
+    cardHeaderStyles = "card-header bg-warning";
+  } else if (props.priority === 3) {
+    priority = "LOW";
+    cardHeaderStyles = "card-header bg-info";
+  }
+
   return (
     <div className="card mb-1 bg-light">
-      <div className="card-header">
-        <strong>ID: {props.projectSequence} -- Priority: {props.priority}</strong>
+      <div className={`d-flex justify-content-between ${cardHeaderStyles}`}>
+        <strong>ID: {props.projectSequence}</strong>
+        <strong>Priority: {priority}</strong>
       </div>
       <div className="card-body bg-light">
         <h5 className="card-title">{props.summary}</h5>
         <p className="card-text text-truncate ">{props.acceptanceCriteria}</p>
-        <Link to={`/projectBoard/${props.projectIdentifier}`} className="btn btn-primary">
+        <Link
+          to={`/projectBoard/${props.projectIdentifier}`}
+          className="btn btn-primary"
+        >
           View / Update
         </Link>
 
--- project-management-tool-react-client/src/components/project-board/project-task/ProjectTaskList.js
+++ project-management-tool-react-client/src/components/project-board/project-task/ProjectTaskList.js
@@ -3,22 +3,30 @@ import React from "react";
 import ProjectTaskItem from "./ProjectTaskItem";
 
 const ProjectTaskList = (props) => {
+  const updateProjectTaskTitleForFiltering = props.title
+    .replace(" ", "_")
+    .toUpperCase();
+  const filteredProjectTasks = props.projectTasks.filter(
+    (projectTask) => projectTask.status === updateProjectTaskTitleForFiltering
+  );
+
   return (
     <div className="col-md-4">
-      <div className={`card text-center mb-2 ${props.headerStyle}`}>
+      <div className={`card text-center mb-2 ${props.cardHeaderStyle}`}>
         <div className="card-header text-white">
           <h3>{props.title}</h3>
         </div>
       </div>
 
-      {props.projectTasks &&
-        props.projectTasks.map((projectTask) => (
+      {filteredProjectTasks &&
+        filteredProjectTasks.map((projectTask) => (
           <ProjectTaskItem
             key={projectTask.id}
             projectSequence={projectTask.projectSequence}
             summary={projectTask.summary}
             acceptanceCriteria={projectTask.acceptanceCriteria}
             priority={projectTask.priority}
+            status={projectTask.status}
           />
         ))}
     </div>
