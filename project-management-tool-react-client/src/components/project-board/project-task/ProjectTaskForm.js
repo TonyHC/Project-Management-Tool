5a602b9c9bd70be918fd27ba2f2b91a1248b8fdd
Tony Chao <tchao@mail.usf.edu>
2022-06-05 12:17:10 -0400

    Update the project task form to create new project task when submit

--- project-management-tool-react-client/src/components/project-board/project-task/ProjectTaskForm.js
+++ project-management-tool-react-client/src/components/project-board/project-task/ProjectTaskForm.js
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { createProjectTask } from "../../../store/project-task-actions";
+
+const initialInputState = {
+  summary: "",
+  acceptanceCriteria: "",
+  dueDate: "",
+  priority: 0,
+  status: ""
+};
 
 const ProjectTaskForm = () => {
+  const [inputState, setInputState] = useState(initialInputState);
+  const dispatch = useDispatch();
+  const history = useHistory();
   const params = useParams();
   const { projectId } = params;
+
+  const userInputHandler = (event) => {
+    const { name, value } = event.target;
+    setInputState((prevInputState) => {
+      return {
+        ...prevInputState,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setInputState(initialInputState);
+    const newProjectTask = {
+      ...inputState,
+      projectIdentifier: projectId
+    };
+
+    dispatch(
+      createProjectTask({
+        projectId: projectId,
+        projectTask: newProjectTask,
+        history: history
+      })
+    );
+  };
 
   return (
     <div className="col-md-8 m-auto">
@@ -13,7 +54,7 @@ const ProjectTaskForm = () => {
       </Link>
       <h4 className="display-4 text-center mt-4">Add / Update Project Task</h4>
       <p className="lead text-center">Project Name + Project Code</p>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="mb-3">
           <label htmlFor="summary" className="form-label">
             Summary
@@ -24,6 +65,8 @@ const ProjectTaskForm = () => {
             name="summary"
             id="summary"
             placeholder="Project Task summary"
+            value={inputState.summary}
+            onChange={userInputHandler}
           />
         </div>
         <div className="mb-3">
@@ -35,6 +78,8 @@ const ProjectTaskForm = () => {
             placeholder="Acceptance Criteria"
             name="acceptanceCriteria"
             id="acceptanceCriteria"
+            value={inputState.acceptanceCriteria}
+            onChange={userInputHandler}
           ></textarea>
         </div>
 
@@ -48,13 +93,21 @@ const ProjectTaskForm = () => {
               className="form-control"
               name="dueDate"
               id="dueDate"
+              value={inputState.dueDate}
+              onChange={userInputHandler}
             />
           </div>
           <div className="col-md-4">
             <label htmlFor="priority" className="form-label">
               Priority
             </label>
-            <select className="form-control" name="priority" id="priority">
+            <select
+              className="form-control"
+              name="priority"
+              id="priority"
+              value={inputState.priority}
+              onChange={userInputHandler}
+            >
               <option value={0}>Select Priority</option>
               <option value={1}>High</option>
               <option value={2}>Medium</option>
@@ -65,7 +118,13 @@ const ProjectTaskForm = () => {
             <label htmlFor="status" className="form-label">
               Status
             </label>
-            <select className="form-control" name="status" id="status">
+            <select
+              className="form-control"
+              name="status"
+              id="status"
+              value={inputState.status}
+              onChange={userInputHandler}
+            >
               <option value="">Select Status</option>
               <option value="TO_DO">TO DO</option>
               <option value="IN_PROGRESS">IN PROGRESS</option>
@@ -84,7 +143,10 @@ const ProjectTaskForm = () => {
           </div>
           <div className="col-md-6">
             <div className="d-grid gap-2">
-              <Link to={`/projectBoard/${projectId}`} className="btn btn-secondary">
+              <Link
+                to={`/projectBoard/${projectId}`}
+                className="btn btn-secondary"
+              >
                 Back to Project Board
               </Link>
             </div>
