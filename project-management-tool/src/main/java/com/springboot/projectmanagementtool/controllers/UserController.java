06556dd3ada09845dd9c0e16abe553bf96244bf8
Tony Chao <tchao@mail.usf.edu>
2022-06-07 15:07:31 -0400

    Add validator for the user entity
    The user validator checks two cases and shows error message if any case fails. First, if password length is less than six characters. Second, if password and confirmPassword fields are the same.
    
    In addition, apply the user validator to the POST endpoint before creating new a user.

--- project-management-tool/src/main/java/com/springboot/projectmanagementtool/controllers/UserController.java
+++ project-management-tool/src/main/java/com/springboot/projectmanagementtool/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.springboot.projectmanagementtool.controllers;
 import com.springboot.projectmanagementtool.domain.User;
 import com.springboot.projectmanagementtool.services.MapValidationErrorService;
 import com.springboot.projectmanagementtool.services.UserService;
+import com.springboot.projectmanagementtool.validator.UserValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -18,14 +19,18 @@ import javax.validation.Valid;
 public class UserController {
     private final MapValidationErrorService mapValidationErrorService;
     private final UserService userService;
+    private final UserValidator userValidator;
 
-    public UserController(MapValidationErrorService mapValidationErrorService, UserService userService) {
+    public UserController(MapValidationErrorService mapValidationErrorService, UserService userService, UserValidator userValidator) {
         this.mapValidationErrorService = mapValidationErrorService;
         this.userService = userService;
+        this.userValidator = userValidator;
     }
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
+        userValidator.validate(user, result);
+
         ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationError(result);
 
         if (errorMap != null) {
