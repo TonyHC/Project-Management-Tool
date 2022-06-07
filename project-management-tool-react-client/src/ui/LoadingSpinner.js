f59383a67ec3191de77d92517b20ddaa00aa49e3
Tony Chao <tchao@mail.usf.edu>
2022-06-06 22:55:06 -0400

    Add a resuable loading spinner component

---
+++ project-management-tool-react-client/src/ui/LoadingSpinner.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import classes from "./LoadingSpinner.module.css";
+
+const LoadingSpinner = () => {
+  return <div className={classes["lds-dual-ring"]}></div>;
+};
+
+export default LoadingSpinner;
