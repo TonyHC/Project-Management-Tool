2cbb2c58e89ac278e1ed88b045633cd2c40e26b9
Tony Chao <tchao@mail.usf.edu>
2022-06-09 19:53:47 -0400

    Update the login component to login a user with validations when form is submit

--- project-management-tool-react-client/src/components/layout/Landing.js
+++ project-management-tool-react-client/src/components/layout/Landing.js
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div class="px-4 py-5 my-5 text-center">
-      <h1 class="display-5 fw-bold">Project Management Tool</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">
+    <div className="px-4 py-5 my-5 text-center">
+      <h1 className="display-5 fw-bold">Project Management Tool</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">
           Create your account to join active projects or start your own
         </p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <Link to="/register" class="btn btn-primary btn-lg px-4 gap-3">
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <Link to="/register" className="btn btn-primary btn-lg px-4 gap-3">
             Sign Up
           </Link>
-          <Link to="/login" class="btn btn-outline-secondary btn-lg px-4">
+          <Link to="/login" className="btn btn-outline-secondary btn-lg px-4">
             Login
           </Link>
         </div>
