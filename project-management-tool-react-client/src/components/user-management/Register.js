75c9d62ec5d7d7f4a07c6b85f15e272e9975734e
Tony Chao <tchao@mail.usf.edu>
2022-06-09 18:27:43 -0400

    Update the register component to create a new user with validations when submit

--- project-management-tool-react-client/src/components/user-management/Register.js
+++ project-management-tool-react-client/src/components/user-management/Register.js
@@ -1,67 +1,133 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import classNames from "classnames";
+
+import { createNewUser } from "../../store/security-actions";
 
 const Register = () => {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { errors } = useSelector((state) => state.security);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const newUser = {
+      firstName: firstNameRef.current.value,
+      lastName: lastNameRef.current.value,
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
+      confirmPassword: confirmPasswordRef.current.value,
+    };
+
+    dispatch(createNewUser({ newUser, history }));
+  };
+
   return (
     <section className="col-md-4 mx-auto mt-5">
-      <div class="card bg-light">
-        <div class="card-body">
-          <h5 class="card-title text-center display-6 mb-4">Register</h5>
+      <div className="card bg-light">
+        <div className="card-body">
+          <h5 className="card-title text-center display-6 mb-4">Register</h5>
 
-          <div class="form-floating mb-3">
-            <input
-              type="text"
-              class="form-control"
-              id="floatingInput"
-              placeholder="Tom"
-            />
-            <label for="floatingInput">First Name</label>
-          </div>
+          <form onSubmit={submitHandler}>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className={classNames("form-control", {
+                  "is-invalid": errors.firstName,
+                })}
+                id="floatingInput"
+                placeholder="Tom"
+                ref={firstNameRef}
+              />
+              <label htmlFor="floatingInput">First Name</label>
 
-          <div class="form-floating mb-3">
-            <input
-              type="text"
-              class="form-control"
-              id="floatingInput"
-              placeholder="Saws"
-            />
-            <label for="floatingInput">Last Name</label>
-          </div>
+              {errors.firstName && (
+                <div className="invalid-feedback">{errors.firstName}</div>
+              )}
+            </div>
 
-          <div class="form-floating mb-3">
-            <input
-              type="email"
-              class="form-control"
-              id="floatingInput"
-              placeholder="name@example.com"
-            />
-            <label for="floatingInput">Email address</label>
-          </div>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className={classNames("form-control", {
+                  "is-invalid": errors.lastName,
+                })}
+                id="floatingInput"
+                placeholder="Saws"
+                ref={lastNameRef}
+              />
+              <label htmlFor="floatingInput">Last Name</label>
 
-          <div class="form-floating mb-3">
-            <input
-              type="password"
-              class="form-control"
-              id="floatingInput"
-              placeholder="password"
-            />
-            <label for="floatingInput">Password</label>
-          </div>
+              {errors.lastName && (
+                <div className="invalid-feedback">{errors.lastName}</div>
+              )}
+            </div>
 
-          <div class="form-floating mb-3">
-            <input
-              type="password"
-              class="form-control"
-              id="floatingInput"
-              placeholder="password"
-            />
-            <label for="floatingInput">Confirm Password</label>
-          </div>
+            <div className="form-floating mb-3">
+              <input
+                type="email"
+                className={classNames("form-control", {
+                  "is-invalid": errors.username,
+                })}
+                id="floatingInput"
+                placeholder="name@example.com"
+                ref={usernameRef}
+              />
+              <label htmlFor="floatingInput">Email address</label>
 
-          <div className="d-grid gap-2">
-            <button type="submit" className="btn btn-primary btn-block mt-2">
-              Register
-            </button>
-          </div>
+              {errors.username && (
+                <div className="invalid-feedback">{errors.username}</div>
+              )}
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className={classNames("form-control", {
+                  "is-invalid": errors.password,
+                })}
+                id="floatingInput"
+                placeholder="password"
+                ref={passwordRef}
+              />
+              <label htmlFor="floatingInput">Password</label>
+
+              {errors.password && (
+                <div className="invalid-feedback">{errors.password}</div>
+              )}
+            </div>
+
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className={classNames("form-control", {
+                  "is-invalid": errors.confirmPassword,
+                })}
+                id="floatingInput"
+                placeholder="password"
+                ref={confirmPasswordRef}
+              />
+              <label htmlFor="floatingInput">Confirm Password</label>
+
+              {errors.confirmPassword && (
+                <div className="invalid-feedback">{errors.confirmPassword}</div>
+              )}
+            </div>
+
+            <div className="d-grid gap-2">
+              <button type="submit" className="btn btn-primary btn-block mt-2">
+                Register
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </section>
