1e24eaa5a7a3ee5ee9a680ba35ef337691dcf5ae
Tony Chao <tchao@mail.usf.edu>
2022-06-10 14:52:56 -0400

    Add auto logout feature to handle the case when the jwt expires
    The auto logout feature only occurs when the user successfully logins and the jwt expires before the user logs out manually.

--- project-management-tool-react-client/src/App.js
+++ project-management-tool-react-client/src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
+import { useDispatch} from "react-redux";
+import jwt_decode from "jwt-decode";
 
 import Dashboard from "./components/Dashboard";
 import ProjectForm from "./components/project/ProjectForm";
@@ -9,40 +11,59 @@ import Layout from "./components/layout/Layout";
 import Landing from "./components/layout/Landing";
 import Login from "./components/user-management/Login";
 import Register from "./components/user-management/Register";
+import { setJWTToken } from "./utils/setJWTToken";
+import { logout } from "./store/security-actions";
+
+const jwt = localStorage.getItem("jwt");
+setJWTToken(jwt);
 
 const App = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();  
+
+  useEffect(() => {
+    if (jwt) {
+      const decoded = jwt_decode(jwt);
+      const currentTime = Date.now() / 1000;
+      
+      if (decoded.exp < currentTime) {
+        dispatch(logout(history));
+      }
+    }
+  }, [dispatch, history]);
+
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <Landing />
-        </Route>
-        <Route path="/dashboard" exact >
-          <Dashboard />
-        </Route>
-        <Route path="/project-form" exact>
-          <ProjectForm />
-        </Route>
-        <Route path="/project-form/:projectId" exact>
-          <ProjectForm />
-        </Route>
-        <Route path="/project-board/:projectId" exact>
-          <ProjectBoard />
-        </Route>
-        <Route path="/project-task-form/:projectId" exact>
-          <ProjectTaskForm />
-        </Route>
-        <Route path="/project-task-form/:projectId/:projectTaskSequence" exact>
-          <ProjectTaskForm />
-        </Route>
-        <Route path="/register" exact>
-          <Register />
-        </Route>
-        <Route path="/login" exact>
-          <Login />
-        </Route>
-      </Switch>
-    </Layout>
+    <Switch>
+      <Route path="/" exact>
+        <Landing />
+      </Route>
+      <Route path="/dashboard" exact >
+        <Dashboard />
+      </Route>
+      <Route path="/project-form" exact>
+        <ProjectForm />
+      </Route>
+      <Route path="/project-form/:projectId" exact>
+        <ProjectForm />
+      </Route>
+      <Route path="/project-board/:projectId" exact>
+        <ProjectBoard />
+      </Route>
+      <Route path="/project-task-form/:projectId" exact>
+        <ProjectTaskForm />
+      </Route>
+      <Route path="/project-task-form/:projectId/:projectTaskSequence" exact>
+        <ProjectTaskForm />
+      </Route>
+      <Route path="/register" exact>
+        <Register />
+      </Route>
+      <Route path="/login" exact>
+        <Login />
+      </Route>
+    </Switch>
+  </Layout>
   );
 };
 
