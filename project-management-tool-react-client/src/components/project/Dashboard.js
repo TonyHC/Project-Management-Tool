c6deaa06cbba71f95d48bb1bd283967fc0825867
Tony Chao <tchao@mail.usf.edu>
2022-06-11 18:10:06 -0400

    Update Dashboard component to dispatch getUserById function once user logs in successfully
    When the user is redirected to the Dashboard component after logging in, the Header component will shortly display the authenticated user's first and last name.

--- project-management-tool-react-client/src/components/project/Dashboard.js
+++ project-management-tool-react-client/src/components/project/Dashboard.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 
 import ProjectList from "./ProjectList";
+import { getUserById } from "../../store/actions/security-actions";
 
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector(state => state.security);
+
+  useEffect(() => {
+    dispatch(getUserById(user.id));
+  }, [dispatch, user.id]);
+
   return (
     <div>
       <h1 className="text-center mb-4">Projects</h1>
