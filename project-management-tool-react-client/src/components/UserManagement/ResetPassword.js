aaff196bd59c858eee3555d76eaa2dae13909c3d
Tony Chao <tchao@mail.usf.edu>
2022-06-13 20:03:54 -0400

    Update the ResetPassword component to display invalidResetPassword error when it occurs

--- project-management-tool-react-client/src/components/UserManagement/ResetPassword.js
+++ project-management-tool-react-client/src/components/UserManagement/ResetPassword.js
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import classNames from "classnames";
 
+import classes from "./ResetPassword.module.css";
+
 const ResetPassword = (props) => {
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
@@ -29,11 +31,17 @@ const ResetPassword = (props) => {
           </h5>
 
           <form onSubmit={submitHandler}>
+            {props.errors.invalidResetPassword && (
+                <div className={classes["invalid-password"]}>
+                  <p>{props.errors.invalidResetPassword}</p>
+                </div>
+            )}
+
             <div className="form-floating mb-3">
               <input
                 type="password"
                 className={classNames("form-control", {
-                  "is-invalid": props.errors.password,
+                  "is-invalid": props.errors.password || props.errors.invalidResetPassword,
                 })}
                 id="floatingInput"
                 placeholder="Password"
@@ -49,7 +57,7 @@ const ResetPassword = (props) => {
               <input
                 type="password"
                 className={classNames("form-control", {
-                  "is-invalid": props.errors.confirmPassword,
+                  "is-invalid": props.errors.confirmPassword || props.errors.invalidResetPassword,
                 })}
                 id="floatingPassword"
                 placeholder="Password"
