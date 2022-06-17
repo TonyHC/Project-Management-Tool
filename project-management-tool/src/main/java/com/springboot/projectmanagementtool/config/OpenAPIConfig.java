59d8fd4e19e8a2d6d553a6409730cbff0f6be1c0
Tony Chao <tchao@mail.usf.edu>
2022-06-16 22:57:32 -0400

    Add OpenAPI config file for springdoc-openapi-ui maven dependency
    Use this maven dependency to document the REST APIs for this web application.

---
+++ project-management-tool/src/main/java/com/springboot/projectmanagementtool/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package com.springboot.projectmanagementtool.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI().info(new Info().title("Project Management Tool")
+                .version("1.0")
+                .description("Sample REST API for Project Management Tool")
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0")
+                        .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
