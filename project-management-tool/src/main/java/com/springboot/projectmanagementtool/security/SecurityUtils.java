02cdf98c5dbf011b4b9a1f5c930684362586a1a9
Tony Chao <tchao@mail.usf.edu>
2022-09-08 20:00:58 -0400

    Update docker-compose file to fix the connection refused issue when logging in to phpmyadmin
    In addition, add a SQL script to create the database and grant all priviledges to the root user.

--- project-management-tool/src/main/java/com/springboot/projectmanagementtool/security/SecurityUtils.java
+++ project-management-tool/src/main/java/com/springboot/projectmanagementtool/security/SecurityUtils.java
@@ -1,12 +1,11 @@
 package com.springboot.projectmanagementtool.security;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityUtils {
-    public  String getAuthenticationUsername() {
+    public String getAuthenticationUsername() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }
